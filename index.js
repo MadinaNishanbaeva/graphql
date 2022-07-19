2620e2f5ad26012753927268898665024c00638f
Madina Nishanbaeva <madina_nishanbaeva@epam.com>
2022-07-19 14:21:08 +0500

    feat: add schema

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('GraphQL is amazing!');
 });
+
+const root = { product: () => {
+  return {
+    "id": 23244,
+    "name": 'widget',
+    "description": 'beautiful widget to use in your garden',
+    "price": 34.99,
+    "soldout": false,
+    "stores": [
+      { store: 'Pasadena' },
+      { store: 'Los Angeles' },
+    ]
+  }
+} };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
